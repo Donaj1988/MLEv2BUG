eab474ad04034a1915fb7baca0dd419284958480
Donaj1988 <Donaj88@proton.me>
2025-08-05 19:55:27 +0200

    restart

--- js/game/save.js
+++
@@ -1,4 +0,0 @@
-const KEY='simple_game_save';
-export function loadSave(){try{return JSON.parse(localStorage.getItem(KEY))||{};}catch{return{}}}
-export function saveGame(state){localStorage.setItem(KEY,JSON.stringify(state));}
-export function autoSave(state){setInterval(()=>saveGame(state),10000);}
